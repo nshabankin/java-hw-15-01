e8b64f772e24be056674f91f1bb99dcb44b94ca6
nshabankin <n.shabankin@gmail.com>
2024-06-05 13:51:18 +0300

    Remove unnecessary loops and method calls

--- src/main/java/ru/netology/javaqa/javaqamvn/manager/Game.java
+++ src/main/java/ru/netology/javaqa/javaqamvn/manager/Game.java
@@ -33,19 +33,24 @@ public class Game {
      */
     public int round(String playerName1, String playerName2) {
 
-        String[] playerNames = {playerName1, playerName2};
+        Player player1 = players.get(playerName1);
+        Player player2 = players.get(playerName2);
 
-        for (String playerName : playerNames) {
-            if (players.get(playerName) == null) {
-                throw new NotRegisteredException(
-                        "Player " + playerName + " not registered"
-                );
-            }
+        if (player1 == null) {
+            throw new NotRegisteredException(
+                    "Player " + playerName1 + " not registered"
+            );
         }
 
-        if (players.get(playerName1).getStrength() < players.get(playerName2).getStrength()) {
+        if (player2 == null) {
+            throw new NotRegisteredException(
+                    "Player " + playerName2 + " not registered"
+            );
+        }
+
+        if (player1.getStrength() < player2.getStrength()) {
             return 2;
-        } else if (players.get(playerName1).getStrength() > players.get(playerName2).getStrength()) {
+        } else if (player1.getStrength() > player2.getStrength()) {
             return 1;
         } else {
             return 0;
